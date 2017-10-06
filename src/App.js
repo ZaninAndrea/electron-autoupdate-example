7e0bc78cc6b9b54190ad00e03a065f65cb706c8c
ZaninAndrea <99.zanin@gmail.com>
2017-10-06 17:46:38 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>Foobar</div>
+    }
+}
+
+export default App;
